a6596e730791220dec77212f085552ff1dd44d38
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-03-14 21:30:05 -0600

    S6E1

--- e17/sesion6/src/main/java/org/bedu/java/backend/sesion6/controller/UsuarioController.java
+++ e17/sesion6/src/main/java/org/bedu/java/backend/sesion6/controller/UsuarioController.java
@@ -1,12 +1,15 @@
 package org.bedu.java.backend.sesion6.controller;
 
+import org.bedu.java.backend.sesion6.model.Direccion;
 import org.bedu.java.backend.sesion6.model.Usuario;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
 public class UsuarioController {
 
+    /*
     @PostMapping
     public String creaUsuario(@RequestBody Usuario usuario) {
         System.out.println("Creando usuario");
@@ -17,6 +20,7 @@ public class UsuarioController {
 
         return "Usuario Creado";
     }
+     */
 
     @PostMapping("/{id}")
     public String creaUsuarioParams(@RequestBody Usuario usuario, @PathVariable("id") long id) {
@@ -39,5 +43,23 @@ public class UsuarioController {
         System.out.println("Rol: " + rol);
 
         return "Usuario " + id + " creado con el rol "+ rol;
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> creaUsuario(@RequestBody Usuario usuario) {
+        System.out.println("Creando usuario");
+        System.out.println("Nombre: " + usuario.getNombre());
+        System.out.println("Apellido: " + usuario.getApellido());
+        System.out.println("Usuario: " + usuario.getUsuario());
+        System.out.println("E-Mail: " + usuario.getCorreoElectronico());
+
+        Direccion direccion = new Direccion();
+        direccion.setCalle("Córdoba");
+        direccion.setNumero("56");
+        direccion.setCodigoPostal("06700");
+
+        usuario.setDireccion(direccion);
+
+        return ResponseEntity.ok(usuario);
     }
 }
