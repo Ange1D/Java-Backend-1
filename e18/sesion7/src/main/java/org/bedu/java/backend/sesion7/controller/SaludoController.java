959a9cadc9c1ef25b7f51527c1fb714b2cbe2f57
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-03-16 21:03:50 -0600

    S7E1

---
+++ e18/sesion7/src/main/java/org/bedu/java/backend/sesion7/controller/SaludoController.java
@@ -0,0 +1,24 @@
+package org.bedu.java.backend.sesion7.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SaludoController {
+
+    @GetMapping("/hola")
+    public ModelAndView hola() {
+        ModelAndView mav = new ModelAndView("hola");
+        mav.addObject("mensaje", "hola desde thymeleaf");
+        return mav;
+    }
+
+    @GetMapping("/holaParam")
+    public ModelAndView holaParam(@RequestParam(name = "mensaje") String mensaje) {
+        ModelAndView mav = new ModelAndView("hola");
+        mav.addObject("mensaje", mensaje);
+        return mav;
+    }
+
+}
